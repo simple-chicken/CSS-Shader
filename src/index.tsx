7c615f1d9f2439d64bf08a354f2d96fd679f8868
hanxuefeng <hanxuefeng@nowcoder.com>
2023-11-07 19:41:07 +0800

    feat: askdbja1
    asdjan1
    
    BREAKING CHANGE: asdj1

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+
+
 // Render your React component instead
 const root = createRoot(document.getElementById("app") as HTMLElement);
 root.render(<App></App>);
