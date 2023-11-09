4e390d461a21f2c8810cdd1a27151e63f0ef0394
hanxuefeng <hanxuefeng@nowcoder.com>
2023-11-09 10:26:07 +0800

    feat(config): 新配置项

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,3 @@
-const czConfig = require('./.cz-config')
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
@@ -6,7 +5,6 @@ module.exports = {
       2,
       'always',
       ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert']
-    ],
-    'scope-enum': [2, 'always', [...czConfig.scopes]] // always 意思是scope必须是数组其中一个，never意思是不可以是数组其中一个
+    ]
   }
 }
